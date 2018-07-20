561ac71183c5a17b1c4f4395ce1d6fe54e8d7fc1
Musa Jahun <msjahun@live.com>
2018-07-20 14:11:31 +0300

    created the java fragments source for the tabs

---
+++ app/src/main/java/com/example/dc/tabbedactivitiesdemo/Tab3Map.java
@@ -0,0 +1,17 @@
+package com.example.dc.tabbedactivitiesdemo;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Map extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_maptab, container, false);
+
+        return rootView;
+    }
+}
